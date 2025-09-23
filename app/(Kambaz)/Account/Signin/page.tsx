b63bf71a21ac8bffb0f4390b67a7312097d80442
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-09-23 00:42:04 -0400

    Initial commit for assignment A1

---
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Signin() {
+  return (
+    <div id="wd-signin-screen">
+      <h3>Sign in</h3>
+      <input placeholder="username" className="wd-username" defaultValue="Sura" /> <br />
+      <input placeholder="password" type="password" className="wd-password" defaultValue="123@#$asd" />
+      <br />
+      <Link id="wd-signin-btn" href="/Dashboard">
+        Sign in
+      </Link>
+      <br />
+      <Link id="wd-signup-link" href="Signup">
+        Sign up
+      </Link>
+    </div>
+  );
+}
