ff94d798d58beeeb86d8db6c84e8d3ffa29b4b3a
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-06 09:31:53 -0400

    Add React Icons sampler component

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -11,6 +11,7 @@ import Zindex from "./Zindex";
 import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
+import ReactIconsSampler from "./ReactIcons";
 
 export default function Lab2() {
   return (
@@ -75,4 +76,5 @@ export default function Lab2() {
       <Float />
       <GridLayout />
       <Flex />
+      <ReactIconsSampler />
     </div>);}
