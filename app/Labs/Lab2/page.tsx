b69d072f9197705139c8b4144bc4f44b47cfae54
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-06 06:36:14 -0400

    Add Z-index example with corresponding CSS in Zindex.tsx and index.css

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -7,6 +7,7 @@ import Margins from "./Margins";
 import Corners from "./Corners";
 import Dimensions from "./Dimensions";
 import Positions from "./Positions";
+import Zindex from "./Zindex";
 
 export default function Lab2() {
   return (
@@ -67,4 +68,5 @@ export default function Lab2() {
       <Corners />
       <Dimensions />
       <Positions />
+      <Zindex />
     </div>);}
