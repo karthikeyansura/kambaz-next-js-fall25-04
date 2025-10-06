34b65e7aec97222e4054c360c62bf5089ca0b7cb
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-06 07:17:58 -0400

    Add CSS float for image wrapping and horizontal layout

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -8,6 +8,7 @@ import Corners from "./Corners";
 import Dimensions from "./Dimensions";
 import Positions from "./Positions";
 import Zindex from "./Zindex";
+import Float from "./Float";
 
 export default function Lab2() {
   return (
@@ -69,4 +70,5 @@ export default function Lab2() {
       <Dimensions />
       <Positions />
       <Zindex />
+      <Float />
     </div>);}
