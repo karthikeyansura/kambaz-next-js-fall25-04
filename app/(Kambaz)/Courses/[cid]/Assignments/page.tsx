c241d64691091f1e836e5504991af403cd52fa98
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-06 21:04:20 -0400

    Add Assignments and Assignment Editor pages

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,52 +1,151 @@
 import Link from "next/link";
+import {
+  Button,
+  InputGroup,
+  FormControl,
+  ListGroup,
+  ListGroupItem,
+  Badge,
+  Container,
+} from "react-bootstrap";
+import { BsGripVertical, BsPlus } from "react-icons/bs";
+import { IoEllipsisVertical, IoSearch } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { FaCheckCircle } from "react-icons/fa";
+import { RxFileText } from "react-icons/rx";
 
 export default function Assignments() {
   return (
-    <div id="wd-assignments">
-      <input placeholder="Search for Assignments" id="wd-search-assignment" />
-      &nbsp;
-      <button id="wd-add-assignment-group">+ Group</button>
-      &nbsp;
-      <button id="wd-add-assignment">+ Assignment</button>
-      <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button>+</button>
-      </h3>
-      <ul id="wd-assignment-list">
-        <li className="wd-assignment-list-item">
-          <Link
-            href="/Courses/1234/Assignments/123"
-            className="wd-assignment-link"
+    <Container id="wd-assignments">
+      <div className="d-flex justify-content-between align-items-center mb-4">
+        <div className="float-start" style={{ width: "300px" }}>
+          <InputGroup className="border rounded">
+            <span className="input-group-text bg-white border-0">
+              <IoSearch />
+            </span>
+            <FormControl
+              placeholder="Search..."
+              id="wd-search-assignment"
+              className="border-0"
+            />
+          </InputGroup>
+        </div>
+        <div>
+          <Button
+            variant="secondary"
+            className="me-2"
+            id="wd-add-assignment-group"
           >
-            A1 - ENV + HTML
-          </Link>
-          <br />
-          Multiple Modules | <strong>Not available until</strong> Sep 6 at
-          12:00am | <strong>Due:</strong> Sep 10 at 11:59pm | 100 pts
-        </li>
-        <li className="wd-assignment-list-item">
-          <Link
-            href="/Courses/1234/Assignments/234"
-            className="wd-assignment-link"
-          >
-            A2 - CSS + BOOTSTRAP
-          </Link>
-          <br />
-          Multiple Modules | <strong>Not available until</strong> Sep 13 at
-          12:00am | <strong>Due:</strong> Sep 17 at 11:59pm | 100 pts
-        </li>
+            <BsPlus className="fs-4" /> Group
+          </Button>
+          <Button variant="danger" id="wd-add-assignment">
+            <BsPlus className="fs-4" /> Assignment
+          </Button>
+        </div>
+      </div>
 
-        <li className="wd-assignment-list-item">
-          <Link
-            href="/Courses/1234/Assignments/345"
-            className="wd-assignment-link"
-          >
-            A3 - JAVASCRIPT + REACT
-          </Link>
-          <br />
-          Multiple Modules | <strong>Not available until</strong> Sep 20 at
-          12:00am | <strong>Due:</strong> Sep 24 at 11:59pm | 100 pts
-        </li>
-      </ul>
-    </div>
+      <ListGroup className="rounded-0">
+        <ListGroupItem className="p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center">
+              <BsGripVertical className="me-2 fs-3" />
+              <IoMdArrowDropdown className="me-2 fs-4" />
+              <strong>ASSIGNMENTS</strong>
+            </div>
+            <div className="d-flex align-items-center">
+              <Badge
+                bg="white"
+                text="dark"
+                className="me-2 rounded-pill px-3 py-2 border"
+              >
+                40% of Total
+              </Badge>
+              <BsPlus className="fs-4" />
+              <IoEllipsisVertical className="fs-4" />
+            </div>
+          </div>
+
+          <ListGroup className="rounded-0">
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <div className="d-flex justify-content-between align-items-start">
+                <div className="d-flex align-items-start">
+                  <BsGripVertical className="me-2 fs-3" />
+                  <RxFileText className="me-3 fs-4 text-success" />
+                  <div>
+                    <Link
+                      href="/Courses/1234/Assignments/123"
+                      className="wd-assignment-link text-dark fw-bold text-decoration-none"
+                    >
+                      A1
+                    </Link>
+                    <div className="text-muted" style={{ fontSize: "0.85rem" }}>
+                      <span className="text-danger">Multiple Modules</span> |{" "}
+                      <strong>Not available until</strong> May 6 at 12:00am |{" "}
+                      <strong>Due</strong> May 13 at 11:59pm | 100 pts
+                    </div>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center">
+                  <FaCheckCircle className="text-success me-2" />
+                  <IoEllipsisVertical className="fs-4" />
+                </div>
+              </div>
+            </ListGroupItem>
+
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <div className="d-flex justify-content-between align-items-start">
+                <div className="d-flex align-items-start">
+                  <BsGripVertical className="me-2 fs-3" />
+                  <RxFileText className="me-3 fs-4 text-success" />
+                  <div>
+                    <Link
+                      href="/Courses/1234/Assignments/234"
+                      className="wd-assignment-link text-dark fw-bold text-decoration-none"
+                    >
+                      A2
+                    </Link>
+                    <div className="text-muted" style={{ fontSize: "0.85rem" }}>
+                      <span className="text-danger">Multiple Modules</span> |{" "}
+                      <strong>Not available until</strong> May 13 at 12:00am |{" "}
+                      <strong>Due</strong> May 20 at 11:59pm | 100 pts
+                    </div>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center">
+                  <FaCheckCircle className="text-success me-2" />
+                  <IoEllipsisVertical className="fs-4" />
+                </div>
+              </div>
+            </ListGroupItem>
+
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <div className="d-flex justify-content-between align-items-start">
+                <div className="d-flex align-items-start">
+                  <BsGripVertical className="me-2 fs-3" />
+                  <RxFileText className="me-3 fs-4 text-success" />
+                  <div>
+                    <Link
+                      href="/Courses/1234/Assignments/345"
+                      className="wd-assignment-link text-dark fw-bold text-decoration-none"
+                    >
+                      A3
+                    </Link>
+                    <div className="text-muted" style={{ fontSize: "0.85rem" }}>
+                      <span className="text-danger">Multiple Modules</span> |{" "}
+                      <strong>Not available until</strong> May 20 at 12:00am |{" "}
+                      <strong>Due</strong> May 27 at 11:59pm | 100 pts
+                    </div>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center">
+                  <FaCheckCircle className="text-success me-2" />
+                  <IoEllipsisVertical className="fs-4" />
+                </div>
+              </div>
+            </ListGroupItem>
+          </ListGroup>
+        </ListGroupItem>
+      </ListGroup>
+    </Container>
   );
 }
