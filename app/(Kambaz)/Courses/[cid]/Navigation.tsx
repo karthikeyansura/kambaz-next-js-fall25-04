cf1059f0e938ad589de7d2996dcc63280042e0c8
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-16 07:34:00 -0400

    feat(a3): Refactor course navigation to be data-driven

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -1,28 +1,30 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useParams } from "next/navigation";
 
 export default function CourseNavigation() {
   const pathname = usePathname();
+  const { cid } = useParams();
+  
   const links = [
-    { id: "wd-course-home-link", path: "Home", label: "Home" },
-    { id: "wd-course-modules-link", path: "Modules", label: "Modules" },
-    { id: "wd-course-piazza-link", path: "Piazza", label: "Piazza" },
-    { id: "wd-course-zoom-link", path: "Zoom", label: "Zoom" },
-    { id: "wd-course-assignments-link", path: "Assignments", label: "Assignments" },
-    { id: "wd-course-quizzes-link", path: "Quizzes", label: "Quizzes" },
-    { id: "wd-course-grades-link", path: "Grades", label: "Grades" },
-    { id: "wd-course-people-link", path: "People/Table", label: "People" },
+    { label: "Home", path: "Home" },
+    { label: "Modules", path: "Modules" },
+    { label: "Piazza", path: "Piazza" },
+    { label: "Zoom", path: "Zoom" },
+    { label: "Assignments", path: "Assignments" },
+    { label: "Quizzes", path: "Quizzes" },
+    { label: "Grades", path: "Grades" },
+    { label: "People", path: "People/Table" },
   ];
+
   return (
-    <div id="wd-courses-navigation" className="list-group wd fs-5 rounded-0" style={{ width: 180 }}>
+    <div id="wd-courses-navigation" style={{ width: 150 }} className="list-group">
       {links.map((link) => (
         <Link
-          key={link.id}
-          href={`/Courses/1234/${link.path}`}
-          id={link.id}
-          className={`list-group-item border-0 ${
-            pathname.includes(link.label) ? "active text-black" : "text-danger"
+          key={link.label}
+          href={`/Courses/${cid}/${link.path}`}
+          className={`list-group-item border border-0 ${
+            pathname.includes(link.label) ? "active text-black" : ""
           }`}
         >
           {link.label}
