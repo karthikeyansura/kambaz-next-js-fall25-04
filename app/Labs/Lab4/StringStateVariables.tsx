03dab05c30a61a816e30e094e15b67ca2aa543f0
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-30 02:12:50 -0400

    refactor(a4): update controlled input logic and consistent useState binding

--- app/Labs/Lab4/StringStateVariables.tsx
+++ app/Labs/Lab4/StringStateVariables.tsx
@@ -1,13 +1,16 @@
-"use client"
 import { useState } from "react";
 import { FormControl } from "react-bootstrap";
 export default function StringStateVariables() {
-  const [firstName, setFirstName] = useState("John");
-  return (
-    <div id="wd-string-state-variables">
-      <h2>String State Variables</h2>
-      <p>{firstName}</p>
-      <FormControl
-        defaultValue={firstName}
-        onChange={(e) => setFirstName(e.target.value)}/>
-<hr/></div>);}
+  const [firstName, setFirstName] = useState("John");
+  return (
+    <div>
+      <h2>String State Variables</h2>
+      <p>{firstName}</p>
+      <FormControl
+        defaultValue={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <hr />
+    </div>
+  );
+}
