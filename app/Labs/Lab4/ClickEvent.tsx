95f3089a58afdf06ce041b8347ea95168278652e
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-30 02:08:53 -0400

    refactor(a4): polish ClickEvent implementation and verify client directive for event handling

--- app/Labs/Lab4/ClickEvent.tsx
+++ app/Labs/Lab4/ClickEvent.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 const hello = () => {
   alert("Hello World!");
 };
@@ -9,17 +9,33 @@ export default function ClickEvent() {
   return (
     <div id="wd-click-event">
       <h2>Click Event</h2>
-      <button onClick={hello} id="wd-hello-world-click">
-        Hello World!</button>
-      <button onClick={() => lifeIs("Good!")}
-              id="wd-life-is-good-click">
-        Life is Good!</button>
-      <button onClick={() => {
-                hello();
-                lifeIs("Great!");
-              }} id="wd-life-is-great-click">
+      <button
+        onClick={hello}
+        id="wd-hello-world-click"
+        className="btn btn-secondary"
+      >
+        Hello World!
+      </button>
+      &nbsp;
+      <button
+        onClick={() => lifeIs("Good!")}
+        id="wd-life-is-good-click"
+        className="btn btn-secondary"
+      >
+        Life is Good!
+      </button>
+      &nbsp;
+      <button
+        onClick={() => {
+          hello();
+          lifeIs("Great!");
+        }}
+        id="wd-life-is-great-click"
+        className="btn btn-secondary"
+      >
         Life is Great!
       </button>
-      <hr/>
+      <hr />
     </div>
-);}
+  );
+}
