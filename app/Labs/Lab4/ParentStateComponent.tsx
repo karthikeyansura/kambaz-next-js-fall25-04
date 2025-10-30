1f318122b7dccdd296109f94578fa2dc6e07054b
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-30 02:12:00 -0400

    refactor(a4): align parent counter state management with consistent UI elements

--- app/Labs/Lab4/ParentStateComponent.tsx
+++ app/Labs/Lab4/ParentStateComponent.tsx
@@ -1,14 +1,12 @@
-"use client"
 import { useState } from "react";
 import ChildStateComponent from "./ChildStateComponent";
 export default function ParentStateComponent() {
-  const [counter, setCounter] = useState(123);
-  return (
-    <div id="wd-parent-state">
-      <h2>Counter {counter}</h2>
-      <ChildStateComponent
-        counter={counter}
-        setCounter={setCounter} />
-      <hr/>
-    </div>
-);}
+  const [counter, setCounter] = useState(123);
+  return (
+    <div>
+      <h2>Counter {counter}</h2>
+      <ChildStateComponent counter={counter} setCounter={setCounter} />
+      <hr />
+    </div>
+  );
+}
