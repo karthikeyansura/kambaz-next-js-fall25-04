08b5266f53f2a49fc660da35b2311bfd97234901
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-10-29 23:33:30 -0400

    feat(a4): add event handling components

---
+++ app/Labs/Lab4/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+
+export default function Lab4() {
+
+  function sayHello() {
+    alert("Hello");
+  }
+
+  return (
+    <div id="wd-lab4">
+      <h3>Lab 4</h3>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+    </div>
+  );
+}
