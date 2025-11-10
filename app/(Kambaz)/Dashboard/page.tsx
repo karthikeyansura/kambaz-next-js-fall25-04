765d2e018763eb2777504de9506daed77b723bdb
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-11-09 22:58:42 -0500

    feat(a4): enable adding new courses with uuid and editable form

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,14 +1,38 @@
 "use client";
+import { useState } from "react";
 import Link from "next/link";
 import * as db from "../Database";
-import { Row, Col, Card, Button } from "react-bootstrap";
+import { Row, Col, Card, Button, FormControl } from "react-bootstrap";
 import Image from "next/image";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Dashboard() {
-  const courses = db.courses;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [course, setCourse] = useState<any>({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg", description: "New Description"
+  });
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: uuidv4() };
+    setCourses([...courses, newCourse ]);
+  }; 
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>New Course
+        <button className="btn btn-primary float-end"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse} > Add </button>
+      </h5><br />
+      <FormControl value={course.name} className="mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      <FormControl as="textarea" value={course.description} rows={3}
+        onChange={(e) => setCourse({ ...course, description: e.target.value }) } />
+      <hr />
       <h2 id="wd-dashboard-published">
         Published Courses ({courses.length})
       </h2>{" "}
