24ff01f294c74790d239882e112e9e65844294c8
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-11-10 00:51:33 -0500

    feat(a4): integrate Redux store and courses reducer for global state management

--- app/(Kambaz)/layout.tsx
+++ app/(Kambaz)/layout.tsx
@@ -1,20 +1,23 @@
+"use client";
 import { ReactNode } from "react";
 import KambazNavigation from "./Navigation";
 import "./styles.css";
+import store from "./store";
+import { Provider } from "react-redux";
 
 export default function KambazLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <div id="wd-kambaz">
-      <div className="d-flex">
+    <Provider store={store}>
+      <div className="d-flex" id="wd-kambaz">
         <div>
           <KambazNavigation />
         </div>
-        <div className="wd-main-content-offset p-3 flex-fill">
+        <div className="flex-fill ps-3 wd-main-content-offset p-3">
           {children}
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
