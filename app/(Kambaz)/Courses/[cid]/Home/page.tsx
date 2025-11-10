eeede0c1ebda04ce53b87a05cf784f6777f6ecab
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-11-10 17:21:47 -0500

    refactor(a4): align home and people pages with Redux state

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,15 +1,18 @@
+import { Container } from "react-bootstrap";
 import Modules from "../Modules/page";
 import CourseStatus from "./Status";
 
 export default function Home() {
   return (
-    <div className="d-flex" id="wd-home">
-      <div className="flex-fill me-3">
-        <Modules />
+    <Container id="wd-home">
+      <div className="d-flex" id="wd-home">
+        <div className="flex-fill me-3">
+          <Modules />
+        </div>
+        <div className="d-none d-lg-block">
+          <CourseStatus />
+        </div>
       </div>
-      <div className="d-none d-lg-block">
-        <CourseStatus />
-      </div>
-    </div>
+    </Container>
   );
 }
