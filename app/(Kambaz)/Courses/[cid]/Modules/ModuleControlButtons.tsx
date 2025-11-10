bcecc4c961276a05453ba1933daf2fa2ea99e4d2
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-11-10 04:15:22 -0500

    refactor(a4): add module editing and migrate module state management to Redux

--- app/(Kambaz)/Courses/[cid]/Modules/ModuleControlButtons.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/ModuleControlButtons.tsx
@@ -1,24 +1,34 @@
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 import { FaTrash } from "react-icons/fa";
-import { BsPlus } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa6";
+import { FaPencil } from "react-icons/fa6";
 
 export default function ModuleControlButtons({
   moduleId,
   deleteModule,
+  editModule,
 }: {
   moduleId: string;
   deleteModule: (moduleId: string) => void;
+  editModule: (moduleId: string) => void;
 }) {
   return (
-<div className="float-end">
+    <div className="float-end">
+      <FaPencil
+        onClick={() => editModule(moduleId)}
+        className="text-primary me-3"
+        style={{ cursor: "pointer" }}
+      />
       <FaTrash
         className="text-danger me-3"
         onClick={() => deleteModule(moduleId)}
         style={{ cursor: "pointer" }}
       />
-      <GreenCheckmark />
-      <BsPlus className="ms-1 fs-1" />
+      <span className="me-2">
+        <GreenCheckmark />
+      </span>
+      <FaPlus className="me-1" />
       <IoEllipsisVertical className="fs-4" />
     </div>
   );
--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,39 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { useState } from "react";
-import { Container, ListGroup } from "react-bootstrap";
+import { Container, ListGroup, FormControl } from "react-bootstrap";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "./LessonControlButtons";
 import ModuleControlButtons from "./ModuleControlButtons";
-import * as db from "../../../Database";
 import { useParams } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../../store";
+import {
+  addModule,
+  editModule,
+  updateModule,
+  deleteModule,
+} from "./reducer";
 
 export default function Modules() {
   const { cid } = useParams();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [modules, setModules] = useState<any[]>(db.modules);
+  const { modules } = useSelector((state: RootState) => state.modulesReducer);
+  const dispatch = useDispatch();
   const [moduleName, setModuleName] = useState("");
-
-  const addModule = () => {
-    setModules([
-      ...modules,
-      { _id: uuidv4(), name: moduleName, course: cid, lessons: [] },
-    ]);
-    setModuleName("");
-  };
-
-  const deleteModule = (moduleId: string) => {
-    setModules(modules.filter((m) => m._id !== moduleId));
-  };
 
   return (
     <Container>
       <ModulesControls
         setModuleName={setModuleName}
         moduleName={moduleName}
-        addModule={addModule}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }}
       />
       <br />
       <br />
@@ -41,22 +39,41 @@ export default function Modules() {
       <br />
       <ListGroup className="rounded-0" id="wd-modules">
         {modules
-          .filter((module) => module.course === cid)
-          .map((module) => (
+          .filter((module: any) => module.course === cid)
+          .map((module: any) => (
             <ListGroup.Item
               key={module._id}
               className="wd-module p-0 mb-5 fs-5 border-gray"
             >
               <div className="wd-title p-3 ps-2 bg-secondary">
-                <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
+                <BsGripVertical className="me-2 fs-3" />
+                {!module.editing && module.name}
+                {module.editing && (
+                  <FormControl
+                    className="w-50 d-inline-block"
+                    onChange={(e) =>
+                      dispatch(
+                        updateModule({ ...module, name: e.target.value })
+                      )
+                    }
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        dispatch(updateModule({ ...module, editing: false }));
+                      }
+                    }}
+                    defaultValue={module.name}
+                  />
+                )}
                 <ModuleControlButtons
                   moduleId={module._id}
-                  deleteModule={deleteModule}
+                  deleteModule={(moduleId) => {
+                    dispatch(deleteModule(moduleId));
+                  }}
+                  editModule={(moduleId) => dispatch(editModule(moduleId))}
                 />
               </div>
               {module.lessons && (
                 <ListGroup className="wd-lessons rounded-0">
-                  {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
                   {module.lessons.map((lesson: any) => (
                     <ListGroup.Item
                       key={lesson._id}
