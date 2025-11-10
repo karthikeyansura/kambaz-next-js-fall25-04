0570cc94a5744804b1e1770e62fe1e1e6adc2686
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-11-10 01:22:53 -0500

    refactor(a4): refactor Courses layout to use Redux and add sidebar toggle functionality

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,33 +1,39 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useState } from "react";
 import CourseNavigation from "./Navigation";
-import { courses } from "../../Database";
-import Breadcrumb from "./Breadcrumb";
+import { useSelector } from "react-redux";
+import { useParams } from "next/navigation";
+import { RootState } from "../../store";
+import { FaAlignJustify } from "react-icons/fa6";
 
-export default async function CoursesLayout({
-  children,
-  params,
-}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
-  const { cid } = await params;
-  const course = courses.find((course) => course._id === cid);
+export default function CoursesLayout({ children }: { children: ReactNode }) {
+  const { cid } = useParams();
+  const { courses } = useSelector((state: RootState) => state.coursesReducer);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const course = courses.find((course: any) => course._id === cid);
+  const [isNavVisible, setIsNavVisible] = useState(true);
 
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
-        <Breadcrumb course={course} />
+        <FaAlignJustify
+          className="me-4 fs-4 mb-1"
+          onClick={() => setIsNavVisible(!isNavVisible)}
+          style={{ cursor: "pointer" }}
+        />
+        {course?.name}
       </h2>
       <hr />
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top" style={{ width: 170 }}>
-              <CourseNavigation />
-            </td>
-            <td valign="top" style={{ width: "100%" }}>
-              {children}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="d-flex">
+        {isNavVisible && (
+          <div style={{ width: 170 }}>
+            <CourseNavigation />
+          </div>
+        )}
+        <div className="flex-fill" style={{ width: "100%" }}>
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
