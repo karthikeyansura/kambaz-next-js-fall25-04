dfe69296d1c5069a5bf6ca73939ba35e8f102bf4
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-11-11 01:15:44 -0500

    -nit fix

--- app/Labs/Lab4/BooleanStateVariables.tsx
+++ app/Labs/Lab4/BooleanStateVariables.tsx
@@ -7,6 +7,7 @@ export default function BooleanStateVariables() {
       <p>{done ? "Done" : "Not done"}</p>
       <label className="form-control">
         <input type="checkbox" checked={done} onChange={() => setDone(!done)} />
+        {" "}
         Done
       </label>
       {done && <div className="alert alert-success">Yay! you are done</div>}
